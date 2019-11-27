f4e043b820da20cb16491ba9aef87d534ccce00e
Randomnicode <randomnicode@gmail.com>
2019-11-27 10:04:07 -0800

    Remove extraneous annotations

--- airsonic-main/src/main/java/org/airsonic/player/security/GlobalSecurityConfig.java
+++ airsonic-main/src/main/java/org/airsonic/player/security/GlobalSecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -81,7 +80,6 @@ public class GlobalSecurityConfig extends GlobalAuthenticationConfigurerAdapter 
     }
 
     @Configuration
-    @EnableWebSecurity
     @Order(1)
     public class ExtSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -117,7 +115,6 @@ public class GlobalSecurityConfig extends GlobalAuthenticationConfigurerAdapter 
     }
 
     @Configuration
-    @EnableWebSecurity
     @Order(2)
     public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
