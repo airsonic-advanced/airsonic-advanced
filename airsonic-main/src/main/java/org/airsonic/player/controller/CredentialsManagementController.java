56dc2bdde9ceb5b22257e5ff8ed6a5bf79e51655
Randomnicode <randomnicode@gmail.com>
2020-01-30 04:30:03 -0800

    Add controller for creds admin

--- airsonic-main/src/main/java/org/airsonic/player/controller/CredentialsManagementController.java
+++ airsonic-main/src/main/java/org/airsonic/player/controller/CredentialsManagementController.java
@@ -3,6 +3,7 @@ package org.airsonic.player.controller;
 import com.google.common.collect.ImmutableMap;
 
 import org.airsonic.player.command.CredentialsManagementCommand;
+import org.airsonic.player.command.CredentialsManagementCommand.AdminControls;
 import org.airsonic.player.command.CredentialsManagementCommand.AppCredSettings;
 import org.airsonic.player.command.CredentialsManagementCommand.CredentialsCommand;
 import org.airsonic.player.domain.User;
@@ -33,7 +34,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.groups.Default;
 
 import java.security.Principal;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -56,7 +56,8 @@ public class CredentialsManagementController {
             App.LASTFM, new AppCredSettings(true, false),
             App.LISTENBRAINZ, new AppCredSettings(false, false));
 
-    private static final Map<String, Object> ENCODER_ALIASES = ImmutableMap.of("noop", "plaintext");
+    private static final Map<String, String> ENCODER_ALIASES = ImmutableMap.of("noop", "plaintext", "legacynoop",
+            "legacyplaintext (deprecated)", "legacyhex", "legacyhex (deprecated)");
 
     @GetMapping
     protected String displayForm() {
@@ -118,11 +119,29 @@ public class CredentialsManagementController {
         map.addAttribute("nonDecodableEncodersJson", Util.toJson(GlobalSecurityConfig.NONLEGACY_NONDECODABLE_ENCODERS));
         map.addAttribute("encoderAliases", ENCODER_ALIASES);
         map.addAttribute("encoderAliasesJson", Util.toJson(ENCODER_ALIASES));
-        map.addAttribute("defaultDecodableEncoder", "hex");
-        map.addAttribute("defaultEncoder", settingsService.getAirsonicPasswordEncoder());
 
-        map.addAttribute("adminRole", userInDb.isAdminRole());
+        map.addAttribute("preferredEncoderNonDecodableAllowed", securityService.getPreferredPasswordEncoder(true));
+        map.addAttribute("preferredEncoderDecodableOnly", securityService.getPreferredPasswordEncoder(false));
+
         map.addAttribute("ldapAuthEnabledForUser", userInDb.isLdapAuthenticated());
+        map.addAttribute("adminRole", userInDb.isAdminRole());
+
+        // admin restricted, installation-wide settings
+        if (userInDb.isAdminRole()) {
+            map.addAttribute("adminControls",
+                    new AdminControls(
+                            securityService.checkCredentialsStoredInLegacyTables(),
+                            securityService.checkLegacyCredsPresent(),
+                            securityService.checkOpenCredsPresent(),
+                            securityService.checkDefaultAdminCredsPresent(),
+                            settingsService.getJWTKey(),
+                            settingsService.getEncryptionPassword(),
+                            settingsService.getEncryptionSalt(),
+                            settingsService.getNonDecodablePasswordEncoder(),
+                            settingsService.getDecodablePasswordEncoder(),
+                            settingsService.getPreferNonDecodablePasswords()
+                            ));
+        }
     }
 
     @PostMapping
@@ -188,5 +207,63 @@ public class CredentialsManagementController {
         redirectAttributes.addFlashAttribute("settings_toast", failures.isEmpty());
 
         return "redirect:credentialsSettings.view";
+    }
+
+    @PostMapping(path = "/admin")
+    protected String adminControls(Authentication user, @Validated @ModelAttribute("adminControls") AdminControls ac,
+            BindingResult br, RedirectAttributes redirectAttributes, ModelMap map) {
+        if (br.hasErrors()) {
+            return "/credentialsSettings";
+        }
+
+        if (map.getAttribute("adminRole") == null || !((boolean) map.getAttribute("adminRole"))) {
+            return "/credentialsSettings";
+        }
+
+        boolean success = true;
+        if (ac.getPurgeCredsInLegacyTables()) {
+            success = securityService.purgeCredentialsStoredInLegacyTables();
+        }
+
+        if (ac.getMigrateLegacyCredsToNonLegacyDefault()) {
+            success = securityService.migrateLegacyCredsToNonLegacy(false);
+        } else if (ac.getMigrateLegacyCredsToNonLegacyDecodableOnly()) {
+            success = securityService.migrateLegacyCredsToNonLegacy(true);
+        }
+
+        boolean saveSettings = false;
+        if (ac.getJwtKeyChanged()) {
+            settingsService.setJWTKey(ac.getJwtKey());
+            saveSettings = true;
+        }
+
+        if (ac.getEncryptionKeyChanged()) {
+            settingsService.setEncryptionPassword(ac.getEncryptionKey());
+            saveSettings = true;
+        }
+
+        if (ac.getNonDecodableEncoderChanged()) {
+            settingsService.setNonDecodablePasswordEncoder(ac.getNonDecodableEncoder());
+            saveSettings = true;
+        }
+
+        if (ac.getDecodableEncoderChanged()) {
+            settingsService.setDecodablePasswordEncoder(ac.getDecodableEncoder());
+            saveSettings = true;
+        }
+
+        if (ac.getNonDecodablePreferenceChanged()) {
+            settingsService.setPreferNonDecodablePasswords(ac.getPreferNonDecodable());
+            saveSettings = true;
+        }
+
+        if (saveSettings) {
+            settingsService.save();
+        }
+
+        redirectAttributes.addFlashAttribute("settings_reload", false);
+        redirectAttributes.addFlashAttribute("settings_toast", success);
+
+        return "redirect:/credentialsSettings.view";
     }
 }
