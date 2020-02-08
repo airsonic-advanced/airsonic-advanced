e15ae92798a4cbda4b6026325b47d1161f216446
Randomnicode <randomnicode@gmail.com>
2020-02-07 22:42:54 -0800

    Add tracking ids to pojos

--- airsonic-main/src/main/java/org/airsonic/player/ajax/NowPlayingInfo.java
+++ airsonic-main/src/main/java/org/airsonic/player/ajax/NowPlayingInfo.java
@@ -19,6 +19,21 @@
  */
 package org.airsonic.player.ajax;
 
+import org.airsonic.player.domain.Avatar;
+import org.airsonic.player.domain.AvatarScheme;
+import org.airsonic.player.domain.MediaFile;
+import org.airsonic.player.domain.PlayStatus;
+import org.airsonic.player.domain.Player;
+import org.airsonic.player.domain.TransferStatus;
+import org.airsonic.player.domain.UserSettings;
+import org.airsonic.player.service.SettingsService;
+import org.airsonic.player.util.StringUtil;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.time.Duration;
+import java.util.UUID;
+
 /**
  * Details about what a user is currently listening to.
  *
@@ -26,7 +41,9 @@ package org.airsonic.player.ajax;
  */
 public class NowPlayingInfo {
 
+    private final UUID transferId;
     private final Integer playerId;
+    private final Integer mediaFileId;
     private final String username;
     private final String artist;
     private final String title;
@@ -40,7 +57,16 @@ public class NowPlayingInfo {
 
     public NowPlayingInfo(Integer playerId, String user, String artist, String title, String tooltip, String streamUrl, String albumUrl,
                           String lyricsUrl, String coverArtUrl, String avatarUrl, int minutesAgo) {
+        this(null, playerId, null, user, artist, title, tooltip, streamUrl, albumUrl, lyricsUrl, coverArtUrl, avatarUrl,
+                minutesAgo);
+    }
+
+    public NowPlayingInfo(UUID transferId, Integer playerId, Integer mediaFileId, String user, String artist, String title,
+            String tooltip, String streamUrl, String albumUrl, String lyricsUrl, String coverArtUrl, String avatarUrl,
+            int minutesAgo) {
+        this.transferId = transferId;
         this.playerId = playerId;
+        this.mediaFileId = mediaFileId;
         this.username = user;
         this.artist = artist;
         this.title = title;
@@ -53,8 +79,16 @@ public class NowPlayingInfo {
         this.minutesAgo = minutesAgo;
     }
 
+    public UUID getTransferId() {
+        return transferId;
+    }
+
     public Integer getPlayerId() {
         return playerId;
+    }
+
+    public Integer getMediaFileId() {
+        return mediaFileId;
     }
 
     public String getUsername() {
@@ -95,5 +129,59 @@ public class NowPlayingInfo {
 
     public int getMinutesAgo() {
         return minutesAgo;
+    }
+
+    public static NowPlayingInfo createForBroadcast(PlayStatus status, SettingsService settingsService) {
+        String url = "";// NetworkService.getBaseUrl(request);
+
+        Player player = status.getPlayer();
+        MediaFile mediaFile = status.getMediaFile();
+        if (mediaFile == null) {
+            return null;
+        }
+        String username = player.getUsername();
+        if (username == null) {
+            return null;
+        }
+        UserSettings userSettings = settingsService.getUserSettings(username);
+        if (!userSettings.isNowPlayingAllowed()) {
+            return null;
+        }
+
+        long minutesAgo = status.getMinutesAgo();
+        if (minutesAgo > 60) {
+            return null;
+        }
+
+        String artist = mediaFile.getArtist();
+        String title = mediaFile.getTitle();
+        String streamUrl = url + "stream?player=" + player.getId() + "&id=" + mediaFile.getId();
+        String albumUrl = url + "main.view?id=" + mediaFile.getId();
+        String lyricsUrl = null;
+        if (!mediaFile.isVideo()) {
+            lyricsUrl = url + "lyrics.view?artistUtf8Hex=" + StringUtil.utf8HexEncode(artist) + "&songUtf8Hex="
+                    + StringUtil.utf8HexEncode(title);
+        }
+        String coverArtUrl = url + "coverArt.view?size=60&id=" + mediaFile.getId();
+
+        String avatarUrl = null;
+        if (userSettings.getAvatarScheme() == AvatarScheme.SYSTEM) {
+            avatarUrl = url + "avatar.view?id=" + userSettings.getSystemAvatarId();
+        } else if (userSettings.getAvatarScheme() == AvatarScheme.CUSTOM
+                && settingsService.getCustomAvatar(username) != null) {
+            avatarUrl = url + "avatar.view?usernameUtf8Hex=" + StringUtil.utf8HexEncode(username);
+        }
+
+        String tooltip = StringEscapeUtils.escapeHtml(artist) + " &ndash; " + StringEscapeUtils.escapeHtml(title);
+
+        if (StringUtils.isNotBlank(player.getName())) {
+            username += "@" + player.getName();
+        }
+        artist = StringEscapeUtils.escapeHtml(StringUtils.abbreviate(artist, 25));
+        title = StringEscapeUtils.escapeHtml(StringUtils.abbreviate(title, 25));
+        username = StringEscapeUtils.escapeHtml(StringUtils.abbreviate(username, 25));
+
+        return new NowPlayingInfo(status.getTransferId(), player.getId(), mediaFile.getId(), username, artist, title,
+                tooltip, streamUrl, albumUrl, lyricsUrl, coverArtUrl, avatarUrl, (int) minutesAgo);
     }
 }
