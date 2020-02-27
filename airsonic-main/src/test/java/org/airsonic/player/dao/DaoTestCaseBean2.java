ad976b57514f921dedff0c59a2a67a47bf3e5e2e
Randomnicode <randomnicode@gmail.com>
2020-02-27 10:14:28 -0800

    Make dao tests transactional so they rollback after test

--- airsonic-main/src/test/java/org/airsonic/player/dao/DaoTestCaseBean2.java
+++ airsonic-main/src/test/java/org/airsonic/player/dao/DaoTestCaseBean2.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class DaoTestCaseBean2 {
     @ClassRule
     public static final HomeRule airsonicRule = new HomeRule();
