18281e79b220b29fd23ffc746e9f6c3578aa8b37
Randomnicode <randomnicode@gmail.com>
2020-03-05 22:11:30 -0800

    Add stream mp3 test

--- integration-test/src/test/java/org/airsonic/test/StreamIT.java
+++ integration-test/src/test/java/org/airsonic/test/StreamIT.java
@@ -2,8 +2,10 @@ package org.airsonic.test;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.subsonic.restapi.Child;
 
 import java.nio.file.Paths;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
@@ -12,30 +14,31 @@ public class StreamIT {
 
     @Test
     public void testStreamFlacAsMp3() throws Exception {
-        Scanner.uploadToDefaultMusicFolder(
-                Paths.get(this.getClass().getResource("/blobs/stream/dead/input").toURI()),
-                "");
-        Scanner.doScan();
-        String mediaFileId = Scanner.getMediaFilesInMusicFolder().get(0).getId();
-        assertNotNull(mediaFileId);
-
-        byte[] fromServer = Scanner.getMediaFileData(mediaFileId);
-        String expectedBodyResource = String.format("/blobs/stream/dead/responses/1.dat");
-        byte[] expected = IOUtils.toByteArray(StreamIT.class.getResourceAsStream(expectedBodyResource));
-        assertThat(fromServer).containsExactly(expected);
+        testFileStreaming("dead");
     }
 
     @Test
     public void testStreamM4aAsMp3() throws Exception {
+        testFileStreaming("dance");
+    }
+
+    @Test
+    public void testStreaMp3() throws Exception {
+        testFileStreaming("piano");
+    }
+
+    private void testFileStreaming(String file) throws Exception {
         Scanner.uploadToDefaultMusicFolder(
-                Paths.get(this.getClass().getResource("/blobs/stream/dance/input").toURI()),
+                Paths.get(this.getClass().getResource("/blobs/stream/" + file + "/input").toURI()),
                 "");
         Scanner.doScan();
-        String mediaFileId = Scanner.getMediaFilesInMusicFolder().get(0).getId();
+        List<Child> files = Scanner.getMediaFilesInMusicFolder();
+        System.out.println("For file " + file + ": " + Scanner.MAPPER.writeValueAsString(files));
+        String mediaFileId = files.get(0).getId();
         assertNotNull(mediaFileId);
 
         byte[] fromServer = Scanner.getMediaFileData(mediaFileId);
-        String expectedBodyResource = String.format("/blobs/stream/dance/responses/1.dat");
+        String expectedBodyResource = String.format("/blobs/stream/" + file + "/responses/1.dat");
         byte[] expected = IOUtils.toByteArray(StreamIT.class.getResourceAsStream(expectedBodyResource));
         assertThat(fromServer).containsExactly(expected);
     }
