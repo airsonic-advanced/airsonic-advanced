ae5c5946ec10a6697b89338020d30ddc07a1f6f4
Faeb35 <fabian.naef@windowslive.com>
2020-05-14 18:06:03 +0200

    Save playlist dialog to cookie

--- airsonic-main/src/main/webapp/script/utils.js
+++ airsonic-main/src/main/webapp/script/utils.js
@@ -74,3 +74,45 @@ function formatDuration(seconds) {
     if (seconds < 0) { res = '-' + res; }
     return res;
 }
+
+function getCurrentPageName() {
+    return window.location.pathname.split("/").pop();
+}
+
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function getJQueryUiDialogPlaylistSize(origin) {
+    var width = getCookie("dialog-select-playlist-" + origin + "-width");
+    var height = getCookie("dialog-select-playlist-" + origin + "-height");
+    if (!width) {
+        width = 300;
+    }
+    if (!height) {
+        height = 250;
+    }
+    return {width: width, height: height};
+}
+
+function setJQueryUiDialogPlaylistSize(origin, size) {
+    setCookie("dialog-select-playlist-" + origin + "-width", parseInt(size.width), 365);
+    setCookie("dialog-select-playlist-" + origin + "-height", parseInt(size.height), 365);
+}
