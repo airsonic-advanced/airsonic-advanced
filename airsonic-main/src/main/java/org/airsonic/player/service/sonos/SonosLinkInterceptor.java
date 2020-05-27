7875e76f7a23ee3178851559914c0879cdb6a348
Randomnicode <randomnicode@gmail.com>
2020-05-27 10:54:57 -0700

    Modify sonos interceptor to use authmanager and add custom jwtverifier

--- airsonic-main/src/main/java/org/airsonic/player/service/sonos/SonosLinkInterceptor.java
+++ airsonic-main/src/main/java/org/airsonic/player/service/sonos/SonosLinkInterceptor.java
@@ -1,22 +1,34 @@
 package org.airsonic.player.service.sonos;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.common.collect.Sets;
 
 import com.sonos.services._1.Credentials;
-import com.sun.org.apache.xerces.internal.dom.ElementNSImpl;
-import org.airsonic.player.service.SecurityService;
+
+import org.airsonic.player.dao.SonosLinkDao;
+import org.airsonic.player.domain.SonosLink;
+import org.airsonic.player.security.JWTAuthenticationProvider.VerificationCheck;
+import org.airsonic.player.security.JWTAuthenticationToken;
 import org.airsonic.player.service.SettingsService;
+import org.apache.commons.codec.binary.StringUtils;
 import org.apache.cxf.binding.soap.SoapMessage;
 import org.apache.cxf.binding.soap.interceptor.AbstractSoapInterceptor;
 import org.apache.cxf.headers.Header;
 import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.phase.Phase;
+import org.apache.cxf.transport.http.AbstractHTTPDestination;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Node;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -27,14 +39,24 @@ import java.util.Set;
 import static org.airsonic.player.service.sonos.SonosServiceRegistration.AuthenticationType;
 
 /**
- * <p>Interceptor for the Soap sonos, is validate access for all methods (Soap Action) except methods for exchange
- * on set link between airsonic and sonos controller.</p>
+ * <p>
+ * Interceptor for the Soap sonos, validates access for all methods (Soap
+ * Action) except methods for exchanging initial tokens between airsonic and
+ * sonos controller.
+ * </p>
  *
- * <p>The validation can be adapt for different link method from sonos.</p>
+ * <p>
+ * The validation can be adapted for different link methods from sonos.
+ * </p>
  */
 @Component
 public class SonosLinkInterceptor extends AbstractSoapInterceptor {
     private static final Logger LOG = LoggerFactory.getLogger(SonosLinkInterceptor.class);
+
+    public static final String CLAIM_HOUSEHOLDID = "householdid";
+    public static final String CLAIM_LINKCODE = "linkcode";
+
+    // these do not carry creds
     private static Set<String> openMethod = Sets.newHashSet("getAppLink", "getDeviceAuthToken");
 
     public SonosLinkInterceptor() {
@@ -42,7 +64,7 @@ public class SonosLinkInterceptor extends AbstractSoapInterceptor {
     }
 
     @Autowired
-    private SecurityService securityService;
+    private AuthenticationManager authenticationManager;
 
     @Autowired
     private SettingsService settingsService;
@@ -65,26 +87,28 @@ public class SonosLinkInterceptor extends AbstractSoapInterceptor {
             AuthenticationType authenticationType = AuthenticationType.valueOf(settingsService.getSonosLinkMethod());
 
             if (action != null && openMethod.contains(action)) {
-                LOG.debug("Unable to process SOAP message: " + message.toString());
+                LOG.debug("No auth required for SOAP message: {}", message.toString());
 
             } else if (action != null && authenticationType == AuthenticationType.APPLICATION_LINK) {
-
+                HttpServletRequest request = (HttpServletRequest) message.get(AbstractHTTPDestination.HTTP_REQUEST);
                 String sonosLinkToken = getToken(message);
                 if (sonosLinkToken != null) {
-                    securityService.authenticate(sonosLinkToken);
+                    JWTAuthenticationToken token = new JWTAuthenticationToken(null, sonosLinkToken,
+                            request.getRequestURI() + "?" + request.getQueryString());
+                    SecurityContextHolder.getContext().setAuthentication(authenticationManager.authenticate(token));
                 }
             } else if (action != null && authenticationType == AuthenticationType.ANONYMOUS) {
-                securityService.authenticate();
+//                securityService.authenticate();
             } else {
-                LOG.debug("Unable to process SOAP message : " + message.toString());
+                LOG.debug("Unable to process SOAP message: {}", message.toString());
                 throw new SonosSoapFault.LoginUnauthorized();
             }
-        } catch (JAXBException e) {
+        } catch (CredentialsExpiredException e) {
+            throw new SonosSoapFault.AuthTokenExpired();
+        } catch (Exception e) {
             throw new SonosSoapFault.LoginUnauthorized();
         }
-
     }
-
 
     private String getToken(SoapMessage message) throws JAXBException {
         QName creadentialQName = new QName("http://www.sonos.com/Services/1.1", "credentials");
@@ -93,8 +117,7 @@ public class SonosLinkInterceptor extends AbstractSoapInterceptor {
 
         for (Header header : message.getHeaders()) {
             if (creadentialQName.equals(header.getName())) {
-                ElementNSImpl elementNS = (ElementNSImpl) header.getObject();
-                Credentials credentials = unmarshaller.unmarshal(elementNS, Credentials.class).getValue();
+                Credentials credentials = unmarshaller.unmarshal((Node) header.getObject(), Credentials.class).getValue();
 
                 return credentials.getLoginToken().getToken();
             }
@@ -116,4 +139,30 @@ public class SonosLinkInterceptor extends AbstractSoapInterceptor {
 
     }
 
+    /**
+     * Separate class to prevent circular dependency chain This class injects into
+     * GlobalSecurityConfig as a dependency to the JWTAuthProvider JWTAuthProvider
+     * is a dependency for AuthManager AuthManager is a dependency for the
+     * SonosLinkSecurityInterceptor class above
+     *
+     * Verifier -> AuthProvider -> AuthManager -> Interceptor If Verifier and
+     * Interceptor are made the same class, we'll have a circular dependency chain
+     */
+    @Component
+    public static class SonosJWTVerification implements VerificationCheck {
+        @Autowired
+        private SonosLinkDao sonosLinkDao;
+
+        @Override
+        public void check(DecodedJWT jwt) throws InsufficientAuthenticationException {
+            String linkcode = jwt.getClaim(CLAIM_LINKCODE).asString();
+            SonosLink sonosLink = sonosLinkDao.findByLinkcode(linkcode);
+
+            if (!StringUtils.equals(jwt.getSubject(), sonosLink.getUsername())
+                    || !StringUtils.equals(linkcode, sonosLink.getLinkcode())
+                    || !StringUtils.equals(jwt.getClaim(CLAIM_HOUSEHOLDID).asString(), sonosLink.getHouseholdId())) {
+                throw new InsufficientAuthenticationException("Sonos creds not valid");
+            }
+        }
+    }
 }
