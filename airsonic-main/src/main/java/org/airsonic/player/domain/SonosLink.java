6c42042846854a17bc3e612319708c3458d71ea0
Randomnicode <randomnicode@gmail.com>
2020-05-27 22:59:52 -0700

    sonos link equals

--- airsonic-main/src/main/java/org/airsonic/player/domain/SonosLink.java
+++ airsonic-main/src/main/java/org/airsonic/player/domain/SonosLink.java
@@ -27,4 +27,41 @@ public class SonosLink {
     public String getLinkcode() {
         return linkcode;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((householdId == null) ? 0 : householdId.hashCode());
+        result = prime * result + ((linkcode == null) ? 0 : linkcode.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SonosLink other = (SonosLink) obj;
+        if (householdId == null) {
+            if (other.householdId != null)
+                return false;
+        } else if (!householdId.equals(other.householdId))
+            return false;
+        if (linkcode == null) {
+            if (other.linkcode != null)
+                return false;
+        } else if (!linkcode.equals(other.linkcode))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
 }
