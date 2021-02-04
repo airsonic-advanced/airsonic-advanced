151dcd188dbc6521856f887c1cb7a3cf923129ca
Randomnicode <randomnicode@gmail.com>
2021-02-04 13:01:34 -0800

    Fix CORs Origin pattern again

--- airsonic-main/src/main/java/org/airsonic/player/spring/WebsocketConfiguration.java
+++ airsonic-main/src/main/java/org/airsonic/player/spring/WebsocketConfiguration.java
@@ -44,7 +44,7 @@ public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket")
-                // .setAllowedOrigins("*")
+                .setAllowedOriginPatterns("*")
                 .addInterceptors(new ServletRequestCaptureHandshakeInterceptor())
                 .withSockJS()
                 .setClientLibraryUrl("../../script/sockjs-1.5.0.min.js");
