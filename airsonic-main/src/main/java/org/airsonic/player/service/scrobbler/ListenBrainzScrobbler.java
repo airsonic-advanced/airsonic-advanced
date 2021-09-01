570f93ccd1eb885a35b1ad1707d12902e40dca02
Randomnicode <randomnicode@gmail.com>
2021-09-01 16:33:47 -0700

    extraneous import

--- airsonic-main/src/main/java/org/airsonic/player/service/scrobbler/ListenBrainzScrobbler.java
+++ airsonic-main/src/main/java/org/airsonic/player/service/scrobbler/ListenBrainzScrobbler.java
@@ -31,7 +31,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.time.Instant;
