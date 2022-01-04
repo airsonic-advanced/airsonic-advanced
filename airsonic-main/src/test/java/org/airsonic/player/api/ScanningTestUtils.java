ad4caa7dcb2ffaccfd8f8c5a95a206a4e8fd9593
Randomnicode <randomnicode@gmail.com>
2022-01-04 01:08:14 -0800

    Fix tests

--- airsonic-main/src/test/java/org/airsonic/player/api/ScanningTestUtils.java
+++ airsonic-main/src/test/java/org/airsonic/player/api/ScanningTestUtils.java
@@ -2,8 +2,8 @@ package org.airsonic.player.api;
 
 import org.airsonic.player.TestCaseUtils;
 import org.airsonic.player.domain.MusicFolder;
+import org.airsonic.player.service.MediaFolderService;
 import org.airsonic.player.service.MediaScannerService;
-import org.airsonic.player.service.SettingsService;
 
 import java.util.Collections;
 import java.util.List;
@@ -15,20 +15,20 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ScanningTestUtils {
     private static Map<UUID, List<MusicFolder>> map = new ConcurrentHashMap<>();
 
-    public static UUID before(List<MusicFolder> musicFolders, SettingsService settingsService, MediaScannerService mediaScannerService) {
+    public static UUID before(List<MusicFolder> musicFolders, MediaFolderService mediaFolderService, MediaScannerService mediaScannerService) {
         UUID id = UUID.randomUUID();
         map.put(id, musicFolders);
-        musicFolders.forEach(settingsService::createMusicFolder);
+        musicFolders.forEach(mediaFolderService::createMusicFolder);
 
         TestCaseUtils.execScan(mediaScannerService);
 
         return id;
     }
 
-    public static void after(UUID id, SettingsService settingsService) {
+    public static void after(UUID id, MediaFolderService mediaFolderService) {
         Optional.ofNullable(map.remove(id)).orElse(Collections.emptyList()).forEach(f -> {
             // maybe consider deleting media_files too
-            settingsService.deleteMusicFolder(f.getId());
+            mediaFolderService.deleteMusicFolder(f.getId());
         });
     }
 }
