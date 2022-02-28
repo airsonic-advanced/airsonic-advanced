ee9db63232e28517ae9bff5094dbcb38918f9d2c
Randomnicode <randomnicode@gmail.com>
2022-02-27 17:16:35 -0800

    Remove import

--- airsonic-main/src/main/java/org/airsonic/player/util/Util.java
+++ airsonic-main/src/main/java/org/airsonic/player/util/Util.java
@@ -19,7 +19,6 @@
  */
 package org.airsonic.player.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
